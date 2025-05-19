d50ef134e37b3b367df1e51659fdc5ca6ca5c80f
lpow100 <lgn.kocher@gmail.com>
2025-05-19 07:37:38 -0700

    made the baseasdfasdf

--- src/main/java/trinary/stars/journies/ClassManager.java
+++ src/main/java/trinary/stars/journies/ClassManager.java
@@ -1,4 +1,26 @@
 package trinary.stars.journies;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+
 public class ClassManager {
+    private final JavaPlugin plugin;
+
+    public ClassManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean isClass(Player player, String className) {
+        String uuid = plugin.getConfig().getString("classes." + className);
+        return uuid != null && uuid.equalsIgnoreCase(player.getUniqueId().toString());
+    }
+
+    public String getPlayerClass(Player player) {
+        for (String className : plugin.getConfig().getConfigurationSection("classes").getKeys(false)) {
+            if (isClass(player, className)) {
+                return className;
+            }
+        }
+        return "None";
+    }
 }
